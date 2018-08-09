cc344cfbebc4f34f4255ff223fa6dcf803efa78d
Alize Nguyen <alizenguyen@gmail.com>
2018-08-09 16:30:08 -0500

    css styling

--- src/components/QuestionInitial.js
+++ src/components/QuestionInitial.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from "react-router";
+import '../css/QuestionInitial.css'
 
 class QuestionInitial extends Component {
   state = {
@@ -26,12 +27,12 @@ class QuestionInitial extends Component {
         <div>
           <img className="question-avatar" alt="user-avatar" src={Object.values(users)[2]} /> 
         </div>
-        <div>
+        <div className="question-form">
           {authedUser.id === this.props.author
             ? <div className="question-title">You asked:</div>
             : <div className="question-title">{this.props.author} asks:</div>}
           <h3> Would you rather... </h3>
-          <p> {this.props.optionOne} ... <br /> OR ...</p>
+          <p className="question-teaser"> {this.props.optionOne} or ... </p>
           <button className="question-button" onClick={(e) => this.renderQuestion(e)}> View Details </button>
         </div>
         <div className="clearfix"></div>
