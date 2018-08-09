bd1600ed2521e52308adb4b7c64548196ab983ee
Alize Nguyen <alizenguyen@gmail.com>
2018-08-09 13:42:17 -0500

    application duplicated

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# would-you-rather-edited
+# Would You Rather 
+
+"Would You Rather" is a Udacity React Nanodegree project incorporating React with Redux. The project's goal was to improve to improvethe predictability of your application's state, to establish strict rules for getting, listening, and updated the store, and to identify what state should live inside of Redux and what state should live inside of React components. 
+
+[Deployed App](https://obscure-ridge-80406.herokuapp.com/)
+
+## The Setup
+
+* `git clone https://github.com/alizenguyen/would-you-rather.git`
+* cd to correlating folder
+* install all project dependencies with `npm install`
+* start the development server with `npm start`
+
+## How it works & Demo
+
+### Login
+
+The app begins on a login page where you will have the option to select a user from a precomposed list. Once a user is chosen, the page will be redirected to the user homepage. 
+
+<img src="./src/images/login.gif" width="800">
+
+### User Homepage
+
+Once the user is authenticated, the user will be able to toggle between the user's unanswered question and answered questions on his or her homepage, which is located at the root. The unanswered questions are shown by default.
+
+To provide clarity, the chosen user's avatar will always be displayed in the nav bar. 
+
+For unanswered questions, the user will be able to see who asked the question, the question, and the two options to choose from. 
+
+For answered questions, the user will be able to see the question, the two options, the number of people who voted for that option, and what the user has chosen. 
+
+<img src="./src/images/user-home.gif" width="800">
+
+### New Question
+
+The user will also have the ability to ask his or her own questions. After clicking on 'New Question,' the application should show a form with the text 'Would You Rather' and inputs for creating the two options. Upon submitting the form, a new poll should be created, the user will be redirected to the user home page, and the new polling question should appear in the correct category on teh home page. 
+
+<img src="./src/images/new-question.gif" width="800">
+
+### Leaderboard
+
+To bring in some healthy competition, the application contains a leaderboard that available via the /leaderboard route. Each user contains the user's name, user's avatar, number of questions asked, number of questions the user answered, and the total score.
+
+Users are in order of descending order based on the sum of questions asked and questions answered. The more questions the user asks and answers, the higher up the user moves.
+
+<img src="./src/images/leaderboard.gif" width="800">
---
+++ src/utils/_DATA.js
@@ -0,0 +1,225 @@
+import averie from '../images/averie-woodard.jpg'
+import ayo from '../images/ayo-ogun.jpg'
+import erik from '../images/erik-luca.jpg'
+
+let users = {
+  averiewoodard: {
+    id: 'averiewoodard',
+    name: 'Averie Woodard',
+    avatarURL: averie,
+    answers: {
+      "8xf0y6ziyjabvozdd253nd": 'optionOne',
+      "6ni6ok3ym7mf1p33lnez": 'optionOne',
+      "am8ehyc8byjqgar0jgpub9": 'optionTwo',
+      "loxhs1bqm25b708cmbf3g": 'optionTwo'
+    },
+    questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
+  },
+  ayoogun: {
+    id: 'ayoogun',
+    name: 'Ayo Ogun',
+    avatarURL: ayo,
+    answers: {
+      "vthrdm985a262al8qx3do": 'optionOne',
+      "xj352vofupe1dqz9emx13r": 'optionTwo',
+    },
+    questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do'],
+  },
+  erikluca: {
+    id: 'erikluca',
+    name: 'Erik Luca',
+    avatarURL: erik,
+    answers: {
+      "xj352vofupe1dqz9emx13r": 'optionOne',
+      "vthrdm985a262al8qx3do": 'optionTwo',
+      "6ni6ok3ym7mf1p33lnez": 'optionOne'
+    },
+    questions: ['6ni6ok3ym7mf1p33lnez', 'xj352vofupe1dqz9emx13r'],
+  }
+}
+
+let questions = {
+  "8xf0y6ziyjabvozdd253nd": {
+    id: '8xf0y6ziyjabvozdd253nd',
+    author: 'averiewoodard',
+    timestamp: 1467166872634,
+    optionOne: {
+      votes: ['averiewoodard'],
+      text: 'have horrible short term memory',
+    },
+    optionTwo: {
+      votes: [],
+      text: 'have horrible long term memory'
+    }
+  },
+  "6ni6ok3ym7mf1p33lnez": {
+    id: '6ni6ok3ym7mf1p33lnez',
+    author: 'ayoogun',
+    timestamp: 1468479767190,
+    optionOne: {
+      votes: [],
+      text: 'become a superhero',
+    },
+    optionTwo: {
+      votes: ['ayoogun', 'averiewoodard'],
+      text: 'become a supervillian'
+    }
+  },
+  "am8ehyc8byjqgar0jgpub9": {
+    id: 'am8ehyc8byjqgar0jgpub9',
+    author: 'averiewoodard',
+    timestamp: 1488579767190,
+    optionOne: {
+      votes: [],
+      text: 'be telekinetic',
+    },
+    optionTwo: {
+      votes: ['averiewoodard'],
+      text: 'be telepathic'
+    }
+  },
+  "loxhs1bqm25b708cmbf3g": {
+    id: 'loxhs1bqm25b708cmbf3g',
+    author: 'erikluca',
+    timestamp: 1482579767190,
+    optionOne: {
+      votes: [],
+      text: 'be a front-end developer',
+    },
+    optionTwo: {
+      votes: ['averiewoodard'],
+      text: 'be a back-end developer'
+    }
+  },
+  "vthrdm985a262al8qx3do": {
+    id: 'vthrdm985a262al8qx3do',
+    author: 'erikluca',
+    timestamp: 1489579767190,
+    optionOne: {
+      votes: [],
+      text: 'find $50 yourself',
+    },
+    optionTwo: {
+      votes: ['ayoogun'],
+      text: 'have your best friend find $500'
+    }
+  },
+  "xj352vofupe1dqz9emx13r": {
+    id: 'xj352vofupe1dqz9emx13r',
+    author: 'ayoogun',
+    timestamp: 1493579767190,
+    optionOne: {
+      votes: ['ayoogun'],
+      text: 'write JavaScript',
+    },
+    optionTwo: {
+      votes: [],
+      text: 'write Swift'
+    }
+  },
+}
+
+//CREATES SELECT USER VARIABLE TO ASSIGN TO A USER
+let selectUser = null
+
+function generateUID () {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+
+export function _getUsers () {
+  return new Promise((res, rej) => {
+    setTimeout(() => res({...users}), 1000)
+  })
+}
+
+//GETS THE USER SELECTED
+export function _getSelectUser () {
+  return new Promise((res, rej) => {
+    setTimeout(() => res(selectUser), 1000)
+  })
+}
+
+export function _getQuestions () {
+  return new Promise((res, rej) => {
+    setTimeout(() => res({...questions}), 1000)
+  })
+}
+
+function formatQuestion ({ optionOneText, optionTwoText, author }) {
+  return {
+    id: generateUID(),
+    timestamp: Date.now(),
+    author,
+    optionOne: {
+      votes: [],
+      text: optionOneText,
+    },
+    optionTwo: {
+      votes: [],
+      text: optionTwoText,
+    }
+  }
+}
+
+//SAVES THE SELECTED USER THROUGHOUT APP
+export function _saveSelectedUser (user) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      selectUser = user;
+      res(user)}, 1000)
+  })
+}
+
+export function _saveQuestion (question) {
+  return new Promise((res, rej) => {
+    const authedUser = question.author;
+    const formattedQuestion = formatQuestion(question);
+
+    setTimeout(() => {
+      questions = {
+        ...questions,
+        [formattedQuestion.id]: formattedQuestion
+      }
+      
+      users = {
+        ...users,
+        [authedUser]: {
+          ...users[authedUser],
+          questions: users[authedUser].questions.concat([formattedQuestion.id])
+        }
+      }
+
+      res(formattedQuestion)
+    }, 1000)
+  })
+}
+
+export function _saveQuestionAnswer ({ authedUser, qid, answer }) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      users = {
+        ...users,
+        [authedUser]: {
+          ...users[authedUser],
+          answers: {
+            ...users[authedUser].answers,
+            [qid]: answer
+          }
+        }
+      }
+
+      questions = {
+        ...questions,
+        [qid]: {
+          ...questions[qid],
+          [answer]: {
+            ...questions[qid][answer],
+            votes: questions[qid][answer].votes.concat([Object.values(authedUser)[0]])
+          }
+        }
+      }
+
+      res()
+    }, 500)
+  })
+}
